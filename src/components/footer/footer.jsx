16a7c31ac52061297bbd4745203d95f0f224f06a
Kirill <developerpopov@yandex.ru>
2021-03-19 18:20:22 +0300

    Написал тест для футера

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,7 +5,7 @@ import {Routes} from '../../const';
 const Footer = () => (
   <footer className="footer container">
     <Link className="footer__logo-link" to={Routes.MAIN}>
-      <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width={64} height={33} />
+      <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width={64} height={33} data-testid="footer-logo"/>
     </Link>
   </footer>
 );
