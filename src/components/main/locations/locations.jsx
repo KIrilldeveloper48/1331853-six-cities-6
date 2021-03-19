49d3acf564c746ab29ea45d90947dc921571d686
Kirill <developerpopov@yandex.ru>
2021-03-19 19:13:22 +0300

    Написал тесты для компонента Locations

--- src/components/main/locations/locations.jsx
+++ src/components/main/locations/locations.jsx
@@ -14,11 +14,14 @@ const Locations = () => {
   return (
     <section className="locations container">
       <ul className="locations__list tabs__list" >
-        {CITY_LIST.map((item) => <li key={item} className="locations__item">
-          <a className={`locations__item-link tabs__item ${item === city ? `tabs__item--active` : ``}`} >
-            <span onClick={() => menuClickHandler(item)}>{item}</span>
-          </a>
-        </li>)}
+        {CITY_LIST.map((item) => (
+          <li key={item} className="locations__item">
+            <a className={`locations__item-link tabs__item ${item === city ? `tabs__item--active` : ``}`} >
+              <span onClick={() => menuClickHandler(item)}>{item}</span>
+            </a>
+          </li>
+        )
+        )}
       </ul>
     </section>
   );
