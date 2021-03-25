f5aa866e35de26bdc284893eec0cf6dd6e06f24b
Kirill <developerpopov@yandex.ru>
2021-03-25 15:36:05 +0300

    Переписал тест в связи с вынесением части разметки в отдельный компонент

--- src/pages/favorites/favorites.test.js
+++ src/pages/favorites/favorites.test.js
@@ -8,77 +8,108 @@ import Favorites from './favorites';
 
 
 const mockStore = configureStore();
-const testDataWithFilledFavoriteList = {
-  DATA: {
-    isFavoriteListLoaded: true,
-    favoriteList: [
-      {
-        id: 1,
-        city: {
-          name: `Paris`
-        },
-        previewImage: `img/1.png`,
-        price: 120,
-        type: `apartment`,
-        rating: 3,
-        isPremium: true,
-        title: `bad room`,
-        isFavorite: true
-      },
-      {
-        id: 2,
-        city: {
-          name: `Amsterdam`
-        },
-        previewImage: `img/1.png`,
-        price: 120,
-        type: `apartment`,
-        rating: 3,
-        isPremium: false,
-        title: `bad room`,
-        isFavorite: true
-      }
-    ]
-  },
-  USER: {
-    authorizationStatus: `AUTH`,
-    userName: `Kirill`,
-    avatarUrl: `image/avatar.jpg`,
-    errorMessage: ``
-  }
-};
 
-const testDataWithEmptyFavoriteList = {
-  DATA: {
-    isFavoriteListLoaded: true,
-    favoriteList: []
-  },
-  USER: {
-    authorizationStatus: `AUTH`,
-    userName: `Kirill`,
-    avatarUrl: `image/avatar.jpg`,
-    errorMessage: ``
-  }
-};
 describe(`Should render 'Favorite' page correctly with filled or empty favorite list `, () => {
   it(`Render 'Favorites' with filled favorite list`, () => {
     const history = createMemoryHistory();
 
+    const testData = {
+      DATA: {
+        isFavoriteListLoaded: true,
+        favoriteList: [
+          {
+            "bedrooms": 3,
+            "city": {
+              "location": {
+                "latitude": 1,
+                "longitude": 1,
+                "zoom": 1
+              },
+              "name": `Paris`
+            },
+            "description": ``,
+            "goods": [`1`, `2`],
+            "host": {
+              "avatarUrl": ``,
+              "id": 3,
+              "isPro": false,
+              "name": ``
+            },
+            "id": 1,
+            "images": [``, ``],
+            "isFavorite": true,
+            "isPremium": false,
+            "location": {
+              "latitude": 1,
+              "longitude": 1,
+              "zoom": 1
+            },
+            "maxAdults": 1,
+            "previewImage": ``,
+            "price": 100,
+            "rating": 4,
+            "title": ``,
+            "type": ``
+          },
+          {
+            "bedrooms": 3,
+            "city": {
+              "location": {
+                "latitude": 1,
+                "longitude": 1,
+                "zoom": 1
+              },
+              "name": `Amsterdam`
+            },
+            "description": ``,
+            "goods": [`1`, `2`],
+            "host": {
+              "avatarUrl": ``,
+              "id": 2,
+              "isPro": false,
+              "name": ``
+            },
+            "id": 2,
+            "images": [``, ``],
+            "isFavorite": true,
+            "isPremium": false,
+            "location": {
+              "latitude": 1,
+              "longitude": 1,
+              "zoom": 1
+            },
+            "maxAdults": 1,
+            "previewImage": ``,
+            "price": 100,
+            "rating": 4,
+            "title": ``,
+            "type": ``
+          },
+        ]
+      },
+      USER: {
+        authorizationStatus: ``,
+        userName: ``,
+        avatarUrl: ``,
+        errorMessage: ``
+      }
+    };
+
     render(
-        <redux.Provider store={mockStore(testDataWithFilledFavoriteList)}>
+        <redux.Provider store={mockStore(testData)}>
           <Router history={history}>
             <Favorites />
           </Router>
         </redux.Provider>
     );
 
+    expect(screen.getByTestId(`favorites`)).toBeInTheDocument();
     expect(screen.getByTestId(`toast`)).toBeInTheDocument();
     expect(screen.getByTestId(`header`)).toBeInTheDocument();
-    expect(screen.getByTestId(`location-btn-Paris`)).toBeInTheDocument();
-    expect(screen.getByTestId(`location-btn-Amsterdam`)).toBeInTheDocument();
+
     expect(screen.getByTestId(`footer`)).toBeInTheDocument();
-    expect(screen.getByTestId(`card-1`)).toBeInTheDocument();
-    expect(screen.getByTestId(`card-2`)).toBeInTheDocument();
+    expect(screen.getByTestId(`favorite-item-Paris`)).toBeInTheDocument();
+    expect(screen.getByTestId(`favorite-item-Amsterdam`)).toBeInTheDocument();
     expect(screen.getByText(`Saved listing`)).toBeInTheDocument();
 
   });
@@ -86,8 +117,21 @@ describe(`Should render 'Favorite' page correctly with filled or empty favorite 
   it(`Render 'Favorites' with empty favorite list`, () => {
     const history = createMemoryHistory();
 
+    const testData = {
+      DATA: {
+        isFavoriteListLoaded: true,
+        favoriteList: []
+      },
+      USER: {
+        authorizationStatus: ``,
+        userName: ``,
+        avatarUrl: ``,
+        errorMessage: ``
+      }
+    };
+
     render(
-        <redux.Provider store={mockStore(testDataWithEmptyFavoriteList)}>
+        <redux.Provider store={mockStore(testData)}>
           <Router history={history}>
             <Favorites />
           </Router>
