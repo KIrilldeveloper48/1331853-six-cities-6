a37853b518748a30ae5a68dc7da65d659275e35a
Kirill <developerpopov@yandex.ru>
2021-03-26 14:06:04 +0300

    Написал тесты для компоненты Boorkmark

---
+++ src/components/offer-property/bookmark/bookmark.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+
+import Bookmark from './bookmark';
+import userEvent from '@testing-library/user-event';
+
+
+describe(`Test 'Bookmark'`, () => {
+  it(`Render 'Bookmark' when parameters 'isFavorite', is true`, () => {
+
+    render(
+        <Bookmark cardFavorCallback={() => { }} isFavorite={true} id={1}/>
+    );
+
+    expect(screen.getByTestId(`offer-property-bookmark`)).toBeInTheDocument();
+    expect(screen.getByTestId(`offer-property-bookmark`)).toHaveClass(`property__bookmark-button--active`);
+    expect(screen.getByText(`To bookmarks`)).toBeInTheDocument();
+  });
+
+  it(`Render 'Bookmark' when parameters 'isFavorite', is false`, () => {
+
+    render(
+        <Bookmark cardFavorCallback={() => { }} isFavorite={false} id={1}/>
+    );
+
+    expect(screen.getByTestId(`offer-property-bookmark`)).toBeInTheDocument();
+    expect(screen.getByTestId(`offer-property-bookmark`)).not.toHaveClass(`property__bookmark-button--active`);
+  });
+
+  it(`Logic should worked correctly`, () => {
+    const fakeCallback = jest.fn();
+    render(
+        <Bookmark cardFavorCallback={fakeCallback} isFavorite={false} id={1}/>
+    );
+
+    expect(screen.getByTestId(`offer-property-bookmark`)).toBeInTheDocument();
+
+    userEvent.click(screen.getByTestId(`offer-property-bookmark`));
+    expect(fakeCallback).toBeCalled();
+  });
+
+
+});
+
+
